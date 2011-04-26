437b4c94fc0f5834ca505e9535ef46b285b2804d
Matthieu Bouron <matthieu.bouron@smartjog.com>
2011-04-26 12:04:27 +0200

    Add support for kt-toolbox extract action, closes: #4234

---
+++ toolbox2/action/extract/kttoolbox_extract.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from toolbox2.action import Action, ActionException
+from toolbox2.worker.kttoolbox import KTToolboxWorker
+
+
+class KTToolboxActionException(ActionException):
+    pass
+
+
+class KTToolboxAction(Action):
+    """
+    Extract subtitles from a gxf file using kt-toolbox from Keres Technologies.
+    """
+
+    name = 'kttoolbox_extract'
+    engine = 'kt-toolbox'
+    category = 'extract'
+    description = 'kt-toolbox extract tool'
+    required_params = {}
+
+    def __init__(self, log, base_dir, _id, params):
+        Action.__init__(self, log, base_dir, _id, params)
+        self.input_file = None
+        self.output_file = None
+        self.tmp_output_dir = os.path.join(self.tmp_dir, 'extract')
+        self.output_dir = None
+
+        if not os.path.isdir(self.tmp_output_dir):
+            os.makedirs(self.tmp_output_dir)
+
+        self.kttoolbox_params = self.params.get('kt-toolbox', {})
+
+    def _check(self):
+        """
+        #FIXME: Check if input file is a gxf container
+        """
+        pass
+
+    def _setup(self):
+
+        self.input_file = self._get_input_path(1)
+        self.output_dir = os.path.dirname(self.input_file)
+
+        worker = KTToolboxWorker(self.log, self.kttoolbox_params)
+        worker.add_input_file(self.input_file)
+        worker.add_output_file(self.tmp_output_dir)
+        self.workers.append(worker)
+
+    def _finalize(self):
+        index = 1
+        for path, _, files in os.walk(self.tmp_output_dir):
+            for filename in files:
+                self._add_output_tmp_path(index, os.path.join(path, filename))
+                self._add_output_path(index, os.path.join(self.output_dir, filename))
+                index += 1
---
+++ toolbox2/worker/kttoolbox.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from toolbox2.worker import Worker, WorkerException
+
+
+option_map = {
+    'vbi_teletext_line_number': {'-gxf-lt': ''},
+    'original_program_title': {'-stl-op': ''},
+    'original_episode_title': {'-stl-oe': ''},
+    'translated_program_title': {'-stl-tp': ''},
+    'translated_episode_title': {'-stl-te': ''},
+    'translator_name': {'-stl-tn': ''},
+    'translator_contact_details': {'-stl-td': ''},
+    'translator_country_origin': {'-stl-co': ''},
+    'publisher': {'-stl-p': ''},
+    'editor_name': {'-stl-en': ''},
+    'editor_contact_details': {'-stl-ed': ''},
+    'user_defined_area': {'-stl-ud': ''},
+}
+
+
+class KTToolboxWorkerException(WorkerException):
+    pass
+
+
+class KTToolboxWorker(Worker):
+    """
+    kt-toolbox worker.
+    """
+
+    class InputFile(Worker.InputFile):
+        def __init__(self, path, params):
+            Worker.InputFile.__init__(self, path, params)
+
+        def get_args(self):
+            return ['-i', self.path]
+
+    class OutputFile(Worker.OutputFile):
+        def __init__(self, path, params):
+            Worker.OutputFile.__init__(self, path, params)
+
+        def get_args(self):
+            return ['-o', self.path]
+
+    def __init__(self, log, params):
+        Worker.__init__(self, log, params)
+        self.stdout = ''
+        self.stderr = ''
+        self.tool = 'kt-toolbox'
+
+        self.args = params.get('args', [])
+        self.action = params.get('action', 'VBITOSTL')
+        self.options = []
+
+        for key, value in option_map.iteritems():
+            option = value.keys()[0]
+            default = value.values()[0]
+            option_value = params.get(key, default)
+            if option_value != '':
+                self.options.append(option)
+                self.options.append(option_value)
+
+    def _handle_output(self, stdout, stderr):
+        self.stdout += stdout
+        self.stderr += stderr
+
+        res = re.findall('Progress: (\d+)%', self.stdout)
+        if len(res) > 0:
+            progress = int(res[-1])
+            if progress > 99:
+                progress = 99
+            self.progress = progress
+
+    def get_error(self):
+        lines = self.stdout.split('\n')
+        error_lines = []
+        lines.reverse()
+
+        i = 0
+        max_lines = 4
+        for line in lines:
+            if not line == '':
+                i += 1
+                error_lines.append(line)
+            if i >= max_lines:
+                break
+
+        error_lines.reverse()
+        return "\n".join(error_lines)
+
+    def get_args(self):
+        args = []
+
+        args += [self.action]
+
+        for input_file in self.input_files:
+            args += input_file.get_args()
+
+        for output_file in self.output_files:
+            args += output_file.get_args()
+
+        args += self.options
+
+        return args
+
+    def _setup(self, base_dir):
+        pass
