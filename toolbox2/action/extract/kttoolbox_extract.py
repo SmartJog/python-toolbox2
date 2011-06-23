87d294a45427cdde8f4d1c8e8519137b1181dd52
Matthieu Bouron <matthieu.bouron@smartjog.com>
2011-06-23 18:45:23 +0200

    Support track_id_path setting in kttoolbox extract action

--- toolbox2/action/extract/kttoolbox_extract.py
+++ toolbox2/action/extract/kttoolbox_extract.py
@@ -24,13 +24,10 @@ class KTToolboxAction(Action):
     def __init__(self, log, base_dir, _id, params):
         Action.__init__(self, log, base_dir, _id, params)
         self.input_file = None
-        self.output_file = None
-        self.tmp_output_dir = os.path.join(self.tmp_dir, 'extract')
-        self.output_dir = None
         self.kttoolbox_worker = None
 
-        if not os.path.isdir(self.tmp_output_dir):
-            os.makedirs(self.tmp_output_dir)
+        if not os.path.isdir(self.tmp_dir):
+            os.makedirs(self.tmp_dir)
 
         self.kttoolbox_params = self.params.get('kttoolbox', {})
 
@@ -43,16 +40,33 @@ class KTToolboxAction(Action):
     def _setup(self):
 
         self.input_file = self._get_input_path(1)
-        self.output_dir = os.path.dirname(self.input_file)
 
         worker = KTToolboxWorker(self.log, self.kttoolbox_params)
         worker.add_input_file(self.input_file)
-        worker.add_output_file(self.tmp_output_dir)
+        worker.add_output_file(self.tmp_dir)
+
         self.kttoolbox_worker = worker
         self.workers.append(worker)
 
     def _finalize(self):
         index = 1
         for _id, path in self.kttoolbox_worker.stls.iteritems():
+            output = self.kttoolbox_params.get('track_%s_path' % _id, None)
+
+            if output:
+                dest = os.path.realpath(self.tmp_dir + '/' + output)
+                basedir = os.path.dirname(dest)
+
+                # Silent here if output directory is self.tmp_dir
+                try:
+                    os.makedirs(basedir)
+                except OSError:
+                    pass
+
+                os.rename(path, dest)
+                path = dest
+
+            self._add_infos('%s-track_%s_path' % (self.name, _id), path)
+
             self._add_output_path(index, path)
             index = index + 1
