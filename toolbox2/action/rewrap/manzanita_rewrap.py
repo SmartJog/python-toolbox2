7b2f33c35d8aadf67c935320f8ac7a7e01c025b1
Matthieu Bouron <matthieu.bouron@smartjog.com>
2012-03-13 12:35:16 +0100

    Refactor manzanita rewrap action and remove dependency over mediaparser

--- toolbox2/action/rewrap/manzanita_rewrap.py
+++ toolbox2/action/rewrap/manzanita_rewrap.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
-import sys
 
 from toolbox2.action import Action, ActionException
+from toolbox2.action.extract.avinfo_extract import AVInfoAction
 from toolbox2.worker.manzanita import ManzanitaMuxWorker
 from toolbox2.worker.ffmpeg import FFmpegWorker
-
-sys.path.append('/usr/share/libtoolbox')
-import mediaparser
 
 
 class ManzanitaRewrapException(ActionException):
@@ -28,86 +25,11 @@ class ManzanitaRewrapAction(Action):
         self.input_file = None
         self.output_file = None
 
-    def _setup(self):
-
-        # FIXME: more customization needed for output paths
-        # Set input_file
-        self.input_file = self.get_input_ressource(1).get('path')
-        if not self.input_file:
-            raise ManzanitaRewrapException('No path specified for input (index = 1)')
-        nb_video_frames = self.get_input_ressource(1).get('nb_video_frames', 0)
-
-        # Compute tmp output path
-        filename = os.path.basename(self.input_file)
-        filename, _ = os.path.splitext(filename)
-        if self.get_output_ressource(1):
-            extension = self.get_output_ressource(1).get('extension')
-        if not extension:
-            extension = '.ts'
-        output_filename = '%s%s' % (filename, extension)
-
-        self.output_file = os.path.join(self.tmp_dir, output_filename)
-        self.add_output_ressource(1, {'path': self.output_file})
-
-        stream_count = 0
-        video_streams = []
-        audio_streams = []
-
-        # Parse video/audio streams
-        streams = mediaparser.streams(self.input_file)
-
-        for stream in streams['video']:
-            stream_count += 1
-            path = os.path.join(self.tmp_dir, 'stream_%s' % stream_count)
-            if stream['codec'] == 'mpeg1video':
-                path += '.m1v'
-            elif stream['codec'] == 'mpeg2video':
-                path += '.m2v'
-            else:
-                raise ManzanitaRewrapException('Unsuported video codec: %s' % stream['codec'])
-
-            video_streams.append({'path': path, 'infos': stream})
-
-        for stream in streams['audio']:
-            stream_count += 1
-            path = os.path.join(self.tmp_dir, 'stream_%s' % stream_count)
-            args = [
-                ('-y'),
-                ('-ar', stream['samplerate']),
-                ('-ac', stream['channels']),
-                ('-acodec', 'copy'),
-            ]
-            if stream['codec'].startswith('pcm_'):
-                codec = stream['codec'][len('pcm_'):]
-                args += [('-f', codec)]
-            elif stream['codec'] == 'mp2':
-                args += [('-f', 'mp2')]
-            elif stream['codec'] == 'libfaad':
-                path += '.aac'
-            else:
-                raise ManzanitaRewrapException('Unsupported audio codec: %s' % stream['codec'])
-
-            audio_streams.append({'path': path, 'infos': stream, 'demux_args': args})
-
-        # Setup demuxing worker
-        demux = self._new_worker(FFmpegWorker, {'args': []})
-        demux.set_nb_frames(nb_video_frames)
-        demux.add_input_file(self.input_file)
-
-        for video_stream in video_streams:
-            demux.add_output_file(video_stream['path'], {'video_opts': [('-vsync', '0'), ('-vcodec', 'copy'), ('-y')]})
-
-        for audio_stream in audio_streams:
-            demux.add_output_file(audio_stream['path'], {'audio_opts': audio_stream['demux_args']})
-
         if 'manzanita' not in self.params:
             self.params['manzanita'] = {}
 
         if 'global' not in self.params['manzanita']:
             self.params['manzanita']['global'] = {}
-
-        # Setup manzanita muxing worker
-        mux = self._new_worker(ManzanitaMuxWorker, self.params['manzanita']['global'])
 
         if 'stream' not in self.params['manzanita']:
             self.params['manzanita']['stream'] = {}
@@ -118,23 +40,54 @@ class ManzanitaRewrapAction(Action):
         if 'audio' not in self.params['manzanita']['stream']:
             self.params['manzanita']['stream']['audio'] = {}
 
-        for i, video_stream in enumerate(video_streams):
-            index = str(i + 1)
-            params = self.params['manzanita']['stream']['video'].get(index, {})
-            params['type'] = 'video'
-            mux.add_input_file(video_stream['path'], params)
+    def _setup(self):
+        self.input_file = self.get_input_ressource(1).get('path')
+        if not self.input_file:
+            raise ManzanitaRewrapException('No path specified for input (index = 1)')
+        nb_video_frames = int(self.get_input_ressource(1).get('nb_video_frames', 0))
 
-        for i, audio_stream in enumerate(audio_streams):
-            index = str(i + 1)
-            params = self.params['manzanita']['stream']['audio'].get(index, {})
-            params['type'] = 'audio'
-            mux.add_input_file(audio_stream['path'], params)
+        # Compute tmp output path
+        filename = os.path.basename(self.input_file)
+        filename, _ = os.path.splitext(filename)
+        extension = '.ts'
+        try:
+            extension = self.get_output_ressource(1).get('extension', '.ts')
+        except ActionException:
+            # Silent exception is file does not exist
+            pass
+        output_filename = '%s%s' % (filename, extension)
 
-        mux.add_output_file(self.output_file)
+        self.output_file = os.path.join(self.tmp_dir, output_filename)
+        self.add_output_ressource(1, {'path': self.output_file})
+
+        avinfo_action = AVInfoAction(self.log, self.base_dir, self.id)
+        avinfo_action.add_input_ressource(1, {'path': self.input_file})
+        avinfo = avinfo_action.run()
+
+        # Setup ffmpeg demuxer
+        ffmpeg = self._new_worker(FFmpegWorker)
+        ffmpeg.add_input_file(self.input_file, {}, avinfo)
+        ffmpeg.set_nb_frames(nb_video_frames)
+        ffmpeg.demux(self.tmp_dir)
+
+        # Setup mp2tsms muxer
+        mp2tsms = self._new_worker(ManzanitaMuxWorker, self.params['manzanita']['global'])
+
+        indexes = {'audio': 0, 'video': 0}
+        for output in ffmpeg.output_files:
+            if output.type not in ['audio', 'video']:
+                continue
+            index = str(indexes[output.type] + 1)
+            params = self.params['manzanita']['stream'][output.type].get(index, {})
+            params['type'] = output.type
+            mp2tsms.add_input_file(output.path, params)
+            indexes[output.type] += 1
+
+        mp2tsms.add_output_file(self.output_file)
 
         # Add demuxing and muxing workers to worker list
-        self.workers.append(demux)
-        self.workers.append(mux)
+        self.workers.append(ffmpeg)
+        self.workers.append(mp2tsms)
 
     def _finalize(self):
         pass
