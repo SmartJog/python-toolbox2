35aaa8ef91e4732957cee2b548b09d7362af47a6
Alexandre Lemaitre <alexandre.lemaitre@cji.paris>
2020-04-08 18:04:43 +0200

    Release version 0.10.6

--- toolbox2/__init__.py
+++ toolbox2/__init__.py
@@ -12,7 +12,7 @@ from toolbox2.action.transcode import *
 from toolbox2.action.getcapability import *
 
 
-__version__ = '0.10.6~dev'
+__version__ = '0.10.6'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
