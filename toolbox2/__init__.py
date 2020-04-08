3d513a73a8c288849f030eb9f30ed535088a5cfa
Alexandre Lemaitre <alexandre.lemaitre@cji.paris>
2020-04-08 18:03:44 +0200

    New development version

--- toolbox2/__init__.py
+++ toolbox2/__init__.py
@@ -12,7 +12,7 @@ from toolbox2.action.transcode import *
 from toolbox2.action.getcapability import *
 
 
-__version__ = '0.10.5'
+__version__ = '0.10.6~dev'
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
